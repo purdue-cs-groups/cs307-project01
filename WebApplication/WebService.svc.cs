0bf829b4a66d394f1304a4c3a4a520722b16696a
Matt McCormick <mbmccormick@gmail.com>
2012-01-18 14:55:54 -0500

    Renaming some things.

---
+++ WebApplication/WebService.svc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WebApplication
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WebService" in code, svc and config file together.
+    public class WebService : IWebService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
