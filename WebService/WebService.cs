e653b36364316a8f74c3185dd2402ffdafd40045
Matt McCormick <mbmccormick@gmail.com>
2012-03-02 16:34:14 -0500

    Adding completed code for Create and Update methods.

--- WebService/WebService.cs
+++ WebService/WebService.cs
@@ -7,6 +7,8 @@ using System.ServiceModel.Activation;
 using System.ServiceModel.Web;
 using WebService.Models;
 using WebService.Controllers;
+using System.IO;
+using System.Web.Helpers;
 
 namespace WebService
 {
@@ -24,15 +26,43 @@ namespace WebService
         #region User Methods
 
         [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/users/fetch?id={id}")]
-        public User GetUser(string id)
+        public User FetchUser(string id)
         {
             return UserController.Fetch(id);
         }
 
         [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/users/fetch")]
-        public List<User> GetUsers()
+        public List<User> FetchAllUsers()
         {
             return UserController.FetchAll();
+        }
+
+        [WebInvoke(Method = "POST", BodyStyle = WebMessageBodyStyle.WrappedRequest, UriTemplate = "/users/create")]
+        public void CreateUser(Stream data)
+        {
+            StreamReader reader = new StreamReader(data);
+            string stringData = reader.ReadToEnd();
+
+            reader.Close();
+            reader.Dispose();
+
+            var jsonData = Json.Decode<User>(stringData);
+
+            UserController.Create(jsonData);
+        }
+
+        [WebInvoke(Method = "POST", BodyStyle = WebMessageBodyStyle.WrappedRequest, UriTemplate = "/users/update")]
+        public void UpdateUser(Stream data)
+        {
+            StreamReader reader = new StreamReader(data);
+            string stringData = reader.ReadToEnd();
+
+            reader.Close();
+            reader.Dispose();
+
+            var jsonData = Json.Decode<User>(stringData);
+
+            UserController.Update(jsonData);
         }
 
         [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/users/delete?id={id}")]
