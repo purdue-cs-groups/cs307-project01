36762dbc311ca981018a82f769c2074bd5434a4e
Joe Martella <martellaj@gmail.com>
2012-03-25 15:25:28 -0400

    Follow/Unfollow on UserDetailPage.

--- MobileApplication/UserDetailPage.xaml.cs
+++ MobileApplication/UserDetailPage.xaml.cs
@@ -14,14 +14,17 @@ using Microsoft.Phone.Controls;
 using WinstagramPan.Models;
 
 using Microsoft.Phone.Tasks;
+using Microsoft.Phone.Shell;
 
 namespace WinstagramPan
 {
     public partial class UserDetailPage : PhoneApplicationPage
     {
+        public static bool isFollowing = true;
         public UserDetailPage()
         {
             InitializeComponent();
+            UpdateAppBar();
         }
 
         private void SendEmail_Tap(object sender, System.Windows.Input.GestureEventArgs e)
@@ -30,6 +33,46 @@ namespace WinstagramPan
             em.To = emailTextBlock.Text;
 
             em.Show();
+        }
+
+        private void UpdateAppBar()
+        {
+            ApplicationBar.Buttons.Clear();
+
+            if (isFollowing)
+            {
+                followingStatus.Text = "Following";
+                followingStatus.Margin = new Thickness(60, 17, 0, 0);
+                followingRec.Fill = new SolidColorBrush(Colors.Green);
+
+                ApplicationBarIconButton unfollow = new ApplicationBarIconButton(new Uri("Images/appbar.download.rest.png", UriKind.Relative));
+                unfollow.Text = "unfollow";
+                unfollow.Click += new EventHandler(Unfollow);
+                ApplicationBar.Buttons.Add(unfollow);
+            }
+            else
+            {
+                followingStatus.Text = "Not Following";
+                followingStatus.Margin = new Thickness(44, 17, 0, 0);
+                followingRec.Fill = new SolidColorBrush(Colors.Red);
+
+                ApplicationBarIconButton follow = new ApplicationBarIconButton(new Uri("Images/appbar.upload.rest.png", UriKind.Relative));
+                follow.Text = "follow";
+                follow.Click += new EventHandler(Follow);
+                ApplicationBar.Buttons.Add(follow);
+            }
+        }
+
+        private void Follow(object sender, EventArgs e)
+        {
+            isFollowing = true;
+            UpdateAppBar();
+        }
+
+        private void Unfollow(object sender, EventArgs e)
+        {
+            isFollowing = false;
+            UpdateAppBar();
         }
     }
 }
