e75ae244b1b8869b107e73c67d93239fc40af3cb
Joe Martella <martellaj@gmail.com>
2012-03-25 13:50:30 -0400

    Introduce Data Binding to UserSearch page that will help during codebehind.
    Bug: Scrolling keeps resetting.

--- MobileApplication/UserSearch.xaml.cs
+++ MobileApplication/UserSearch.xaml.cs
@@ -29,5 +29,36 @@ namespace WinstagramPan
         {
             InitializeComponent();
         }
+
+        public static ObservableCollection<User> SearchResults = new ObservableCollection<User>();
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            User u = new User();
+            u.Name = "Joe Martella";
+            u.Username = "martellaj";
+            SearchResults.Add(u);
+
+            User u2 = new User();
+            u2.Name = "Matt McCormick";
+            u2.Username = "mbmccormick";
+            SearchResults.Add(u2);
+
+            User u3 = new User();
+            u3.Name = "James Ma";
+            u3.Username = "jimmymama";
+            SearchResults.Add(u3);
+
+            User u4 = new User();
+            u4.Name = "Josh William";
+            u4.Username = "jdawg";
+            SearchResults.Add(u4);
+
+            User u5 = new User();
+            u5.Name = "Jeremiah Theurer";
+            u5.Username = "jerryThizzle";
+            SearchResults.Add(u5);
+
+            searchResults.ItemsSource = SearchResults;
+        }
     }
 }
