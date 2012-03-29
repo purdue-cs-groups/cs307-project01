ff87b41bfde1d85d4613fa9aaad86842e3756f09
Jeremiah Theurer <jrtheurer@gmail.com>
2012-03-29 19:44:27 -0400

    Adding the relationship model and controller to github.

---
+++ WebService/Controllers/RelationshipController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebService.Models;
+using MongoDB.Driver;
+using WebService.Common;
+using MongoDB.Bson;
+
+namespace WebService.Controllers
+{
+    public class RelationshipController
+    {
+        //Fetches a list of all the users following the user with given id.
+        public static Relationship FetchRaltions(string uid)
+        {
+            MongoServer server = MongoServer.Create(Global.DatabaseConnectionString);
+            MongoDatabase database = server.GetDatabase(Global.DatabaseName);
+
+            MongoCollection<Relationship> relationship = database.GetCollection<Relationship>("Relationship");
+            var query = new QueryDocument("uid", uid);
+
+            return relationship.FindOne(query);
+        }
+
+        //Deletes a user from the list of followers for the given id.
+        public static Boolean DeleteFollower(string uid, string follower)
+        {
+            MongoServer server = MongoServer.Create(Global.DatabaseConnectionString);
+            MongoDatabase database = server.GetDatabase(Global.DatabaseName);
+
+            MongoCollection<BsonDocument> relationship = database.GetCollection<BsonDocument>("Relationship");
+            var query = new QueryDocument
+            {
+                { "uid", uid}
+            };
+            var update = new UpdateDocument
+            {
+                {"$pull", new BsonDocument("followers", follower)}
+            };
+            SafeModeResult updateRelation = relationship.Update(query, update);
+            if (updateRelation.LastErrorMessage == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //Deletes a user from the list of users following the given id.
+        public static Boolean DeleteFollowing(string uid, string following)
+        {
+            MongoServer server = MongoServer.Create(Global.DatabaseConnectionString);
+            MongoDatabase database = server.GetDatabase(Global.DatabaseName);
+
+            MongoCollection<BsonDocument> relationship = database.GetCollection<BsonDocument>("Relationship");
+            var query = new QueryDocument
+            {
+                { "uid", uid}
+            };
+            var update = new UpdateDocument
+            {
+                {"$pull", new BsonDocument("following", following)}
+            };
+            SafeModeResult updateRelation = relationship.Update(query, update);
+            if (updateRelation.LastErrorMessage == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //Adds a user to the list of users following the given id.
+        public static Boolean AddFollower(string uid, string follower)
+        {
+            MongoServer server = MongoServer.Create(Global.DatabaseConnectionString);
+            MongoDatabase database = server.GetDatabase(Global.DatabaseName);
+
+            MongoCollection<BsonDocument> relationship = database.GetCollection<BsonDocument>("Relationship");
+            var query = new QueryDocument
+            {
+                { "uid", uid}
+            };
+            var update = new UpdateDocument
+            {
+                {"$addToSet", new BsonDocument("followers", follower)}
+            };
+            SafeModeResult updateRelation = relationship.Update(query, update);
+            if (updateRelation.LastErrorMessage == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //Adds a user to the list of users the given id is following.
+        public static Boolean AddFollowing(string uid, string following)
+        {
+            MongoServer server = MongoServer.Create(Global.DatabaseConnectionString);
+            MongoDatabase database = server.GetDatabase(Global.DatabaseName);
+
+            MongoCollection<BsonDocument> relationship = database.GetCollection<BsonDocument>("Relationship");
+            var query = new QueryDocument
+            {
+                { "uid", uid}
+            };
+            var update = new UpdateDocument
+            {
+                {"$addToSet", new BsonDocument("following", following)}
+            };
+            SafeModeResult updateRelation = relationship.Update(query, update);
+            if (updateRelation.LastErrorMessage == null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ WebService/Models/Relationship.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using System.Runtime.Serialization;
+using WebService.Common;
+namespace WebService.Models
+{
+    [DataContract]
+    public class Relationship
+    {
+        [DataMember(Order = 0)]
+        public string Followers
+        {
+            get;
+            set;
+        }
+
+        [DataMember(Order = 1)]
+        public string Following
+        {
+            get;
+            set;
+        }
+    }
+}
