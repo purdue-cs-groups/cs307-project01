abcf0fe80bf5aba469054141ba92b4b6e2494fb6
Jeremiah Theurer <jrtheurer@gmail.com>
2012-03-30 01:37:23 -0400

    Added the Relationship and UserConnectedAccount Models to the MobileClientLibrary.

---
+++ MobileClientLibrary/Models/Relationship.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MobileClientLibrary.Common;
+
+namespace ClientLibrary.Models
+{
+    public class Relationship
+    {
+        public string UID
+        {
+            get;
+            set;
+        }
+
+        public string Followers
+        {
+            get;
+            set;
+        }
+
+        public string Following
+        {
+            get;
+            set;
+        }
+    }
+}
