3830f6c40a9cc26544809fe916975f73a2514af6
James Ma <jamesma.1987@gmail.com>
2012-04-02 16:37:00 -0400

    Created int userid Settings object.

--- MobileApplication/Settings/Settings.cs
+++ MobileApplication/Settings/Settings.cs
@@ -15,13 +15,30 @@ namespace MetrocamPan
       public static readonly Setting<string> password =
           new Setting<string>("Password", "");
 
+      public static readonly Setting<int> userid =
+          new Setting<int>("Userid", -1);
+
+      // Setting for save original photo into media library
       public static readonly Setting<bool> saveOriginal =
           new Setting<bool>("SaveOriginal", false);
 
+      // Setting for save edited photo into media library
       public static readonly Setting<bool> saveEdited =
           new Setting<bool>("SaveEdited", false);
 
       public static readonly Setting<bool> locationService =
           new Setting<bool>("LocationService", false);
+
+      // Reset all Setting objects to default values
+      public static void resetToDefault()
+      {
+          isLoggedIn.Value = false;
+          username.Value = "";
+          password.Value = "";
+          userid.Value = -1;
+          saveOriginal.Value = false;
+          saveEdited.Value = false;
+          locationService.Value = false;
+      }
   }
 }
