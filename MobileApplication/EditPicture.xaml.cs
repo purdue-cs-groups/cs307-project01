c97316bea2fc3210555a68ca1bbd761daec089ee
Joe Martella <martellaj@gmail.com>
2012-04-02 08:44:02 -0400

    Put code in place to apply filters in app (Antique).

--- MobileApplication/EditPicture.xaml.cs
+++ MobileApplication/EditPicture.xaml.cs
@@ -11,32 +11,80 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 
+using PictureEffects;
+using PictureEffects.CompositeEffects;
+using PictureEffects.Effects;
+using MetrocamPan.Models;
+using System.Windows.Media.Imaging;
+
 namespace MetrocamPan
 {
     public partial class EditPicture : PhoneApplicationPage
     {
+        private ImageSource capturedSource;
+        public ImageSource CapturedSource
+        {
+            get 
+            {
+                return capturedSource;
+            }
+            set
+            {
+                capturedSource = value;
+            }
+        }
+
         public EditPicture()
         {
             InitializeComponent();
         }
 
+        public static WriteableBitmap bitmap = null;
         public static Image editedPicture = new Image();
         private void capturedImage_Loaded(object sender, RoutedEventArgs e)
         {
             if (!MainPage.isLandscape)
             {
-                capturedImage.Source = CropPage.cropped.Source;
+                capturedSource = CropPage.cropped.Source;
             }
             else
             {
-                capturedImage.Source = CropPageLandscapeOrientation.cropped.Source;
+                capturedSource = CropPageLandscapeOrientation.cropped.Source;
             }
+
+            bitmap = new WriteableBitmap((BitmapSource)capturedSource);
+            capturedImage.Source = CapturedSource;
         }
 
         private void Check_Click(object sender, EventArgs e)
         {
             editedPicture.Source = capturedImage.Source;
             NavigationService.Navigate(new Uri("/UploadPage.xaml", UriKind.Relative));
+        }
+
+        private void RemoveEffect(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            capturedImage.Source = bitmap;
+            capturedImage.InvalidateArrange();
+            capturedImage.InvalidateMeasure();
+        }
+
+        private void ApplyAntique(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            EffectItem item = new EffectItem(new AntiqueEffect());
+            IEffect effect = item.Effect;
+
+            if (bitmap == null) return;
+
+            var width = bitmap.PixelWidth;
+            var height = bitmap.PixelHeight;
+            var resultPixels = effect.Process(bitmap.Pixels, width, height);
+
+            WriteableBitmap newBitmap = resultPixels.ToWriteableBitmap(width, height);
+
+            capturedImage.Source = newBitmap;
+            capturedImage.InvalidateArrange();
+            capturedImage.InvalidateMeasure();
         }
     }
 }
---
+++ MobileApplication/Models/EffectItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+using PictureEffects.Effects;
+
+namespace MetrocamPan.Models
+{
+    /// <summary>
+    /// A generic effect item.
+    /// </summary>
+    public class EffectItem
+    {
+        public IEffect Effect { get; private set; }
+        public string Name { get; private set; }
+
+        public EffectItem(IEffect effect)
+        {
+            Effect = effect;
+            Name = effect.Name;
+        }
+    }
+}
