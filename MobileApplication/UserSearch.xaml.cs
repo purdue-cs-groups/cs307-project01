983e3755e9105000a814586d4c700121a7ae164d
Josh <jdlwilliams@gmail.com>
2012-04-10 14:49:26 -0400

    Initial implementation of user search.  Profile picture is not yet displayed on the search results, but name and user name are.  Linking to the profile is not complete either.

--- MobileApplication/UserSearch.xaml.cs
+++ MobileApplication/UserSearch.xaml.cs
@@ -31,37 +31,72 @@ namespace MetrocamPan
             InitializeComponent();
         }
 
-        public static ObservableCollection<User> SearchResults = new ObservableCollection<User>();
-        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        protected virtual void OnNavigatedTo(NavigationEventArgs e)
         {
+            noresults.Visibility = System.Windows.Visibility.Collapsed;
+            searchResults.Visibility = System.Windows.Visibility.Collapsed;
             SearchResults.Clear();
 
-            User u = new User();
-            u.Name = "Joe Martella";
-            u.Username = "martellaj";
-            SearchResults.Add(u);
-
-            User u2 = new User();
-            u2.Name = "Matt McCormick";
-            u2.Username = "mbmccormick";
-            SearchResults.Add(u2);
-
-            User u3 = new User();
-            u3.Name = "James Ma";
-            u3.Username = "jimmymama";
-            SearchResults.Add(u3);
-
-            User u4 = new User();
-            u4.Name = "Josh William";
-            u4.Username = "jdawg";
-            SearchResults.Add(u4);
-
-            User u5 = new User();
-            u5.Name = "Jeremiah Theurer";
-            u5.Username = "jerryThizzle";
-            SearchResults.Add(u5);
-
-            searchResults.ItemsSource = SearchResults;
         }
+
+        public static ObservableCollection<User> SearchResults = new ObservableCollection<User>();
+        public static List<UserInfo> results;
+        private void searchbutton_Click(object sender, EventArgs e)
+        {
+            searchResults.Visibility = System.Windows.Visibility.Collapsed;
+            noresults.Visibility = System.Windows.Visibility.Collapsed;
+            App.MetrocamService.SearchUsersCompleted +=new MobileClientLibrary.RequestCompletedEventHandler(MetrocamService_SearchUsersCompleted);
+            App.MetrocamService.SearchUsers(this.searchterms.Text);
+        }
+
+
+        void  MetrocamService_SearchUsersCompleted(object sender, MobileClientLibrary.RequestCompletedEventArgs e)
+        {
+            App.MetrocamService.SearchUsersCompleted -= MetrocamService_SearchUsersCompleted;
+            SearchResults.Clear();
+
+            //link to profile
+            //profile picture
+
+
+            results = e.Data as List<UserInfo>;
+            foreach (UserInfo uio in results) {
+                User result = new User();
+                result.Name = uio.Name;
+                result.Username = uio.Username;
+                SearchResults.Add(result);
+            }
+
+            if (results.Count == 0)
+            {
+                noresults.Visibility = System.Windows.Visibility.Visible;
+            }
+            else {
+                searchResults.ItemsSource = SearchResults;
+                searchResults.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+
+        private void searchterms_GotFocus(object sender, RoutedEventArgs e)
+        {
+            searchResults.Visibility = System.Windows.Visibility.Collapsed;
+            noresults.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        private void ViewUserDetailFromUsername_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            MessageBox.Show("This should navigate to the user detail page.  Butttttt it doesn't.  Just FYI.");
+         /*   TextBlock username = sender as TextBlock;
+            UserInfo info = username.DataContext as UserInfo;
+
+            if (info.ID == null)
+            {
+                MessageBox.Show("the user info object is somehow wrong\n");
+            }
+            else
+            {
+                NavigationService.Navigate(new Uri("/UserDetailPage.xaml?id=" + info.ID + "&type=recent", UriKind.Relative));
+            }*/
+       }
     }
 }
