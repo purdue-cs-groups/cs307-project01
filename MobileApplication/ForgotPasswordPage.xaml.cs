5551b3b2939823368d2faa12577e9d8878908397
James Ma <jamesma.1987@gmail.com>
2012-04-10 21:38:46 -0400

    ForgotPasswordPage.xaml now opens a BrowserControl that redirects to a website link for password retrieval.

--- MobileApplication/ForgotPasswordPage.xaml.cs
+++ MobileApplication/ForgotPasswordPage.xaml.cs
@@ -10,13 +10,13 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
-using System.Windows.Navigation;
-using JeffWilcox.FourthAndMayor;
 
 namespace MetrocamPan
 {
     public partial class ForgotPasswordPage : PhoneApplicationPage
     {
+        private string forgotRedirectionLink = "http://metrocam.cloudapp.net/ResetPassword.aspx";
+
         public ForgotPasswordPage()
         {
             InitializeComponent();
@@ -26,61 +26,7 @@ namespace MetrocamPan
         {
             base.OnNavigatedTo(e);
 
-            App.isFromLandingPage = true;
-        }
-
-        private void usernameInput_KeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                // Switch focus to next input field
-                Dispatcher.BeginInvoke(() =>
-                    emailInput.Focus());
-            }
-        }
-
-        private void emailInput_KeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                // Lose Focus on the keyboard
-                Dispatcher.BeginInvoke(() =>
-                    this.Focus());
-            }
-        }
-
-        private void AboutMenuItem_Click(object sender, EventArgs e)
-        {
-            NavigationService.Navigate(new Uri("/AboutPage.xaml", UriKind.Relative));
-        }
-
-        private void Accept_Click(object sender, EventArgs e)
-        {
-            // Validate input
-            if (!InputValidator.isValidLength(this.usernameInput.Text, "username", InputValidator.usernameLowerBoundary, InputValidator.usernameUpperBoundary) ||
-                !InputValidator.isValidEmail(this.emailInput.Text))
-            {
-                return;
-            }
-
-            Settings.isLoggedIn.Value = true;
-            App.isFromLandingPage = true;
-
-            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
-        }
-
-        private void Cancel_Click(object sender, EventArgs e)
-        {
-            // GoBack() Automatically clears everything on this PortraitPage
-            NavigationService.GoBack();
-        }
-
-        protected override void OnNavigatedFrom(NavigationEventArgs e)
-        {
-            base.OnNavigatedFrom(e);
-
-            if (GlobalLoading.Instance.IsLoading)
-                GlobalLoading.Instance.IsLoading = false;
+            this.BrowserControl.Source = new Uri(forgotRedirectionLink, UriKind.Absolute);
         }
     }
 }
